1a088bbeba22eb1f44dc0514e26936fdb0316653
TwinProduction <twin@twinnation.org>
2020-06-23 22:11:43 -0400

    Update dependencies

--- vendor/github.com/TwinProduction/gdstore/entry.go
+++ vendor/github.com/TwinProduction/gdstore/entry.go
@@ -18,16 +18,8 @@ type Entry struct {
 	Value  []byte
 }
 
-func (e Entry) toLine() []byte {
-	encodedKeyChannel := make(chan string)
-	encodedValueChannel := make(chan string)
-	go func() {
-		encodedKeyChannel <- base64.StdEncoding.EncodeToString([]byte(e.Key))
-	}()
-	go func() {
-		encodedValueChannel <- base64.StdEncoding.EncodeToString(e.Value)
-	}()
-	return []byte(fmt.Sprintf("%s,%s,%s\n", e.Action, <-encodedKeyChannel, <-encodedValueChannel))
+func (e *Entry) toLine() []byte {
+	return []byte(fmt.Sprintf("%s,%s,%s\n", e.Action, base64.StdEncoding.EncodeToString([]byte(e.Key)), base64.StdEncoding.EncodeToString(e.Value)))
 }
 
 func newEntry(action Action, key string, value []byte) *Entry {
@@ -52,7 +44,6 @@ func newBulkEntries(action Action, keyValues map[string][]byte) []*Entry {
 
 func newEntryFromLine(line string) (*Entry, error) {
 	elements := strings.Split(line, ",")
-
 	if len(elements) != 3 {
 		return nil, ErrBadLine
 	}
--- vendor/github.com/TwinProduction/gdstore/gdstore.go
+++ vendor/github.com/TwinProduction/gdstore/gdstore.go
@@ -1,23 +1,49 @@
 package gdstore
 
 import (
+	"bufio"
 	"os"
 	"strconv"
 	"sync"
 )
 
 type GDStore struct {
+	// FilePath is the path to the file used to persist
 	FilePath string
-	file     *os.File
-	data     map[string][]byte
-	mux      sync.Mutex
+
+	// useBuffer lets the user define if GDStore should use a buffer, or write directly to the file.
+	//
+	// Writing to a buffer is much faster, but failure to close to the store (GDStore.Close()) will
+	// result in a buffer that hasn't been flushed, meaning some entries may be lost.
+	// You can manually flush the buffer by using GDStore.Flush().
+	//
+	// In contrast, writing to the file without a buffer is slower, but more reliable if your
+	// application is prone to suddenly crashing
+	//
+	// Defaults to false
+	useBuffer bool
+
+	// persistence lets the user set whether to persist data to the file or not. Meant to be used for testing without
+	// having to clean up the file.
+	//
+	// (default) If set to true, data is available both in-memory and persisted to the file found at FilePath.
+	// If set to false, data is available only in-memory, and upon destruction, all data will be lost.
+	//
+	// Defaults to true
+	persistence bool
+
+	file   *os.File
+	writer *bufio.Writer
+	data   map[string][]byte
+	mux    sync.Mutex
 }
 
 // New creates a new GDStore
 func New(filePath string) *GDStore {
 	store := &GDStore{
-		FilePath: filePath,
-		data:     make(map[string][]byte),
+		FilePath:    filePath,
+		data:        make(map[string][]byte),
+		persistence: true,
 	}
 	err := store.loadFromDisk()
 	if err != nil {
@@ -26,7 +52,26 @@ func New(filePath string) *GDStore {
 	return store
 }
 
+// WithBuffer sets GDStore's useBuffer parameter to the value passed as parameter
+//
+// The default value for useBuffer is false
+func (store *GDStore) WithBuffer(useBuffer bool) *GDStore {
+	store.useBuffer = useBuffer
+	return store
+}
+
+// WithPersistence sets GDStore's persistence parameter to the value passed as parameter
+//
+// The ability to set persistence to false is there mainly for testing purposes
+//
+// The default value for persistence is true
+func (store *GDStore) WithPersistence(persistence bool) *GDStore {
+	store.persistence = persistence
+	return store
+}
+
 // Get returns the value of a key as well as a bool that indicates whether an entry exists for that key.
+//
 // The bool is particularly useful if you want to differentiate between a key that has a nil value, and a
 // key that doesn't exist
 func (store *GDStore) Get(key string) (value []byte, ok bool) {
@@ -37,7 +82,7 @@ func (store *GDStore) Get(key string) (value []byte, ok bool) {
 // GetString does the same thing as Get, but converts the value to a string
 func (store *GDStore) GetString(key string) (valueAsString string, ok bool) {
 	var value []byte
-	value, ok = store.data[key]
+	value, ok = store.Get(key)
 	if ok {
 		valueAsString = string(value)
 	}
@@ -111,16 +156,4 @@ func (store *GDStore) Values() [][]byte {
 		i++
 	}
 	return values
-}
-
-// Close closes the store's file if it isn't already closed.
-// Note that any write actions, such as the usage of Put and PutAll, will automatically re-open the store.
-func (store *GDStore) Close() {
-	if store.file != nil {
-		err := store.file.Close()
-		store.file = nil
-		if err != nil {
-			panic(err)
-		}
-	}
 }
